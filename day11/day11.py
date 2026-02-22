1052075f9711616d8e998d252cdb706b8ae442e6
Dylan Tang <dtang04@uchicago.edu>
2026-02-21 20:36:49 -0600

    Day 11 part 2

--- day11/day11.py
+++ day11/day11.py
@@ -9,6 +9,20 @@ def findAllPaths(current, links):
         numPaths += findAllPaths(n, links)
     return numPaths
 
+def findAllPaths_DACFFT(current, links, trail = ["svr"]):
+    if current == 'out':
+        if 'dac' in trail and 'fft' in trail:
+            return 1
+        return 0
+    neighbors = links[current]
+    if len(neighbors) == 0:
+        return 0
+    numPaths = 0
+    for n in neighbors:
+        if n in trail:
+            continue
+        numPaths += findAllPaths_DACFFT(n, links, trail + [n])
+    return numPaths
 
 def main():
     links = {}
@@ -18,7 +32,8 @@ def main():
             key = lst[0]
             vals = lst[1].strip().split(" ")
             links[key] = vals
-    print(findAllPaths("you", links))
+    print(findAllPaths("you", links)) #Answer: 764
+    print(findAllPaths_DACFFT("svr", links, ["svr"]))
 
 if __name__ == "__main__":
     main()
