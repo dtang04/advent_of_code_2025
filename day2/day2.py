d0b4bd54ea6b4df8c8a4631ed9dd6ad7b4e2466c
Dylan Tang <dtang04@uchicago.edu>
2026-02-23 10:01:33 -0600

    Finished AoC day2

--- day2/day2.py
+++ day2/day2.py
@@ -8,9 +8,26 @@ def checkRepeat(num):
         return True
     return False
 
+def checkRepeat_p2(num):
+    stri = str(num)
+    for chunk_size in range(1, len(stri)):
+        if len(stri) % chunk_size != 0:
+            continue
+        chunks = []
+        for i in range(0, len(stri), chunk_size):
+            chunks.append(stri[i:i+chunk_size])
+        template = chunks[0]
+        satisfy = True
+        for i in range(1,len(chunks)):
+            if chunks[i] != template:
+                satisfy = False
+        if satisfy:
+            return True
+    return False 
 
 def main():
     count = 0
+    count_p2 = 0
     intervals = []
     with open("day2.txt", 'r') as f:
         for line in f:
@@ -25,6 +42,9 @@ def main():
             for i in range(low, high+1):
                 if checkRepeat(i):
                     count += i
+                if checkRepeat_p2(i):
+                    count_p2 += i
     print(count) # Part 1: 31000881061
+    print(count_p2) # Part 2: 46769308485
 if __name__ == "__main__":
     main()
